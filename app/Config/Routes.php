af1d2c722e2a5d6e8c1076959147292816771751
andyblaze <andy@mediagraphic.co.uk>
2025-10-27 14:49:25 +0000

    cms

--- app/Config/Routes.php
+++ app/Config/Routes.php
@@ -5,5 +5,9 @@ use CodeIgniter\Router\RouteCollection;
 /**
  * @var RouteCollection $routes
  */
+// cms updates
 $routes->get('api/files/(:any)', 'Api::files/$1');
+// Default home page
 $routes->get('/', 'Home::index');
+// Catch-all for all other URLs
+$routes->get('(:any)', 'Home::index/$1');
