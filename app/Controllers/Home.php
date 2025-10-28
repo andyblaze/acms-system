cfb3f23d6d5be2a5b3b3dd00e184ff62fc44c7d1
andyblaze <andy@mediagraphic.co.uk>
2025-10-28 12:25:52 +0000

    cms

--- app/Controllers/Home.php
+++ app/Controllers/Home.php
@@ -4,20 +4,31 @@ namespace App\Controllers;
 use App\Models\PagesModel;
 use App\Models\HtmlIdentifiersModel;
 use App\Models\PageTemplatesModel;
+use App\Models\RendererClassesModel;
 use App\Libraries\FormBuilder;
 
 class Home extends BaseController {
-    public function index(): string {
+    protected function buildSelects() {
+        $fields = [
+            'page_id'               => PagesModel::class,
+            'html_identifier_id'    => HtmlIdentifiersModel::class,
+            'template_id'           => PageTemplatesModel::class,
+            'renderer_id'           =>RendererClassesModel::class
+        ];
+
         $f = new FormBuilder();
-        $m = new PagesModel();
         $f->open();
-        $f->select('page_id', $m->asIdValueMap());
-        $f->select('status', $m->enumValues());
-        $m = new HtmlIdentifiersModel();
-        $f->select('html_identifier_id', $m->asIdValueMap());
-        $m = new PageTemplatesModel();
-        $f->select('template_id', $m->asIdValueMap());
-        echo $f->close();
+
+        // loop through the fields
+        foreach ( $fields as $field => $modelClass ) {
+            $m = new $modelClass();
+            $f->select($field, $m->asIdValueMap());
+        }
+        return $f->close();
+    }
+    public function index(): string {
+        //$f->select('status', $m->enumValues());
+        echo $this->buildSelects();
         return view('pages/' . $this->viewData['view_file'], $this->viewData);
     }
 }
---
+++ app/Models/RendererClassesModel.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace App\Models;
+
+use App\Models\CmsBaseModel;
+
+class RendererClassesModel extends CmsBaseModel {
+    protected $table = 'renderer_classes';
+    protected $valueField = 'renderer_class';
+}
