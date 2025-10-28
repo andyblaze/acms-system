c1700dee6c1fc9b21200ab21d83c11f072640307
andyblaze <andy@mediagraphic.co.uk>
2025-10-28 13:48:02 +0000

    cms

--- gitpush.bat
+++ gitpush.bat
@@ -3,7 +3,8 @@ if "%~1"=="" (
   echo Usage: gitpush.bat "commit message"
   exit /b 1
 )
-
+echo Backing up database...
+"C:\Program Files\MySQL\MySQL Server 8.0\bin\mysqldump" -u roe09 -proe09 ___acms > backup.sql
 git add .
 git commit -m "%~1"
 git push origin main
